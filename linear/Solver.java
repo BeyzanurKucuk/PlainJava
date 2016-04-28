683c3772599d4979d40c8be46b08fe5981a9f910
Mehmet Eyler <ma.eyler@gmail.com>
2016-04-28 06:44:33 +0300

    add button

--- linear/Solver.java
+++ linear/Solver.java
@@ -12,19 +12,20 @@ public class Solver implements ListSelectionListener, Runnable {
     static final float 
         RES_RATIO = RESOLUTION/96f;  //default resolution is 96
     static final int H = scaled(30), W = scaled(80), GAP = scaled(8);
-    static final Font NORMAL = scaledFont("Dialog", 0, 14);
-    static final Font LARGE = scaledFont("Dialog", 0, 18);
+    static final Font LARGE = scaledFont("Dialog", 0, 16);
+    static final Font NORMAL = scaledFont("Dialog", 0, 13);
+    static final Font SMALL = scaledFont("Dialog", 0, 10);
     
     final Matrix mat;
-    final JFrame frm = new JFrame("Solver");
-    final JLabel lab = new JLabel(MSG);
-    final JTable tab = new JTable(mat);
-    final JLabel msg = new JLabel(mat.toString());
+    final JTable tab;
+    final JFrame frm;
+    JLabel lab, msg, res;
     
     public Solver(Matrix m) {
         mat = m;
         JPanel pan = new JPanel(new BorderLayout(GAP, GAP));
         
+        tab = new JTable(mat);
         tab.setRowHeight(H);
         tab.setFont(LARGE);
         JTableHeader head = tab.getTableHeader();
@@ -36,27 +37,51 @@ public class Solver implements ListSelectionListener, Runnable {
         cr.setHorizontalAlignment(JLabel.CENTER);
 
         JScrollPane scr = new JScrollPane(tab);
+        Dimension d = head.getPreferredSize();
+        System.out.println(d);
         int w = mat.getColumnCount() * W;
-        int h = (mat.getRowCount() + 1) * H;
+        int h = mat.getRowCount() * H + (d.height+3);
         scr.setPreferredSize(new Dimension(w, h));
         tab.getSelectionModel().addListSelectionListener(this);
         pan.add(scr, "Center");
         
+        lab = new JLabel(MSG);
         lab.setFont(NORMAL);
         lab.setHorizontalAlignment(SwingConstants.CENTER);
         pan.add(lab, "North");
-        msg.setFont(NORMAL);
-        msg.setHorizontalAlignment(SwingConstants.CENTER);
-        pan.add(msg, "South");
+        
+        pan.add(bottomPanel(), "South");
         
         pan.setBorder(new EmptyBorder(GAP, GAP, GAP, GAP));
         tab.setToolTipText("Cofficients");
         msg.setToolTipText("The result");
 
+        frm = new JFrame("Solver");
         frm.setContentPane(pan); 
         frm.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         //frm.setLocation(90, 50);
         frm.pack(); frm.setVisible(true);
+    }
+    JPanel bottomPanel() {
+        JPanel bot = new JPanel();
+        bot.setLayout(new BoxLayout(bot, BoxLayout.X_AXIS));
+        msg = new JLabel("x: exchange  +: add row");
+        msg.setFont(SMALL);
+        msg.setHorizontalAlignment(SwingConstants.LEFT);
+        bot.add(msg);
+        
+        bot.add(Box.createHorizontalGlue());
+        
+        res = new JLabel(mat.toString());
+        res.setFont(NORMAL);
+        res.setHorizontalAlignment(SwingConstants.CENTER);
+        bot.add(res);
+        bot.add(Box.createHorizontalStrut(H));
+        
+        JButton b = new JButton("New");
+        b.setFont(NORMAL);
+        bot.add(b);
+        return bot;
     }
     public void valueChanged(ListSelectionEvent e) {
         if (e.getValueIsAdjusting()) return;
@@ -67,14 +92,14 @@ public class Solver implements ListSelectionListener, Runnable {
         while (!done) {
             done = mat.forward(k);
             tab.getSelectionModel().setSelectionInterval(k, k);
-            k++; msg.setText("Row "+k);
+            k++; res.setText("Row "+k);
             //System.out.println("Row "+k);
             tab.repaint();
             try { Thread.sleep(1500);
             } catch (InterruptedException e) {
             }
         }
-        msg.setText("Determinant = "+mat.det);
+        res.setText("Determinant = "+mat.det);
         if (mat.getRowCount() == mat.getColumnCount()) return;
         //mat.backward(); tab.repaint(); 
     }
